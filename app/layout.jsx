0e8568f785f0ac195f702a43a6c0e826b468ec35
Colm Hewson <colmhewson@Colms-MacBook-Pro.local>
2023-05-23 00:26:34 +0100

    Added viewport meta globally

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,6 +5,9 @@ import Head from 'next/head';
 export default function RootLayout({ children }) {
     return (
       <html lang="en">
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        </Head>
         <body className={styles.home}>{children}</body>
       </html>
     );
