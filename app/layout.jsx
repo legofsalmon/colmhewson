921a8ff91840c4853833ad747b36f84625411061
Colm Hewson <colmhewson@Colms-MacBook-Pro.local>
2023-05-23 00:09:45 +0100

    updated homepage to image template

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,11 @@
 import '../styles/global/global.scss';
+import styles from './layout.module.scss';
+import Head from 'next/head';
 
 export default function RootLayout({ children }) {
     return (
       <html lang="en">
-        <body>{children}</body>
+        <body className={styles.home}>{children}</body>
       </html>
     );
   }      
--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,18 @@
 import Link from 'next/link';
+import Image from 'next/image';
+import styles from './layout.module.scss';
 
 export default function Page() {
   return (
+    <>
       <h1>Colm Hewson</h1>
-      
+      <Image
+        src="/images/stock/test-stock-image.jpeg"
+        width={500}
+        height={500}
+        alt="Picture of the author"
+      />
+      <div className={styles["colour-wash"]}></div>
+    </>
   );
 }
